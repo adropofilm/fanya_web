9e010efc5fc127ef96d67e93b803c2d39f30395f
Fatima Mohamed <adropofilm@gmail.com>
2022-10-13 02:12:14 -0500

    create input form & add state handling

--- src/newTaskInputForm/NewTaskInputForm.js
+++ src/newTaskInputForm/NewTaskInputForm.js
@@ -1,10 +1,26 @@
-import React from "react";
-import './styles.css'
+import React, { useState } from "react";
+import './styles.css';
+import { TextField } from "@mui/material";
 
 const NewTaskInputForm = () => {
+    const [taskString, setTask] = useState(null)
+
+    const onFormSubmit = (task) => {
+        task.preventDefault();
+        setTask(task);
+    }
+
     return (
-        <form>
-            <input id="new-task-input" type="text" required/>
+        <form onSubmit={() => onFormSubmit}>
+            <TextField 
+                onChange={(text) => {
+                    setTask(text.target.value);
+                    console.log(text.target.value)
+                }}
+                id="filled-basic" 
+                label="Filled" 
+                variant="filled" 
+                required/>
         </form>
     )
 }
