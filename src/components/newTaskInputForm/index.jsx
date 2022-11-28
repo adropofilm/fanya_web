8ac5a20620e7547f15f701b9695a468ed3ab11ed
Fatima Mohamed <adropofilm@gmail.com>
2022-11-27 23:10:34 -0600

    Configure jest and add support for css modules
    Co-authored-by: Cris <rcmtcristian@gmail.com>
    Co-authored-by: Jason Jugo <HondaChan14@users.noreply.github.com>

---
+++ src/components/newTaskInputForm/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import styles from './styles.module.css';
+
+const NewTaskInputForm = ({getAllTasks}) => {
+    const [taskString, setTask] = useState("")
+
+    let body = {
+        title: taskString,
+        status: "open"
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const createNewTask = async () => {
+            await axios.post(`${process.env.REACT_APP_API_HOST}/tasks`, body)
+            getAllTasks();
+        };
+        createNewTask();
+        setTask("");
+    }
+
+    return (
+
+        <form id={styles["new-task-input-form"]} className="flex-column-center" onSubmit={onFormSubmit}>
+            <input 
+                id={styles["new-task-input"]}
+                placeholder="Type the name of your task..."
+                onChange={(text) => {
+                    setTask(text.target.value);
+                }}
+                type="text"
+                value={taskString}
+                required />
+
+            <button id={styles["new-task-btn"]} type="submit">create task</button>
+        </form>
+    )
+}
+
+export default NewTaskInputForm;
