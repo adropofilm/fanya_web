f849222e529616b6b8ec3ab19e86c0977186ba3c
Fatima Mohamed <adropofilm@gmail.com>
2022-12-11 23:41:57 -0600

    mock getAllTasks functions in component tests

--- src/components/newTaskInputForm/NewTaskInputForm.test.tsx
+++ src/components/newTaskInputForm/NewTaskInputForm.test.tsx
@@ -1,7 +1,11 @@
 import renderer from "react-test-renderer";
 import { NewTaskInputForm } from "./NewTaskInputForm";
 
+const getAllTasks = jest.fn();
+
 it("renders correctly", () => {
-  const tree = renderer.create(<NewTaskInputForm />).toJSON();
+  const tree = renderer
+    .create(<NewTaskInputForm getAllTasks={getAllTasks} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/taskItem/TaskItem.test.tsx
+++ src/components/taskItem/TaskItem.test.tsx
@@ -1,9 +1,18 @@
 import renderer from "react-test-renderer";
 import { TaskItem } from "./TaskItem";
 
+const getAllTasks = jest.fn();
+
 it("renders correctly", () => {
   const tree = renderer
-    .create(<TaskItem id={1} status="open" title="clean the garage" />)
+    .create(
+      <TaskItem
+        id={1}
+        status="open"
+        title="clean the garage"
+        getAllTasks={getAllTasks}
+      />
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
