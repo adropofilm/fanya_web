b79c13fe126177d03b0711e90c214771029107a4
Fatima Mohamed <adropofilm@gmail.com>
2022-12-26 23:27:07 -0600

    remove unnecessary prop from test components

--- src/components/taskItem/TaskItem.test.tsx
+++ src/components/taskItem/TaskItem.test.tsx
@@ -1,18 +1,11 @@
 import renderer from "react-test-renderer";
-import { Status } from "../../types/Task.types";
+import { TaskStatus } from "../../types/Task.types";
 import { TaskItem } from "./TaskItem";
-
-const setTasks = jest.fn();
 
 it("renders correctly", () => {
   const tree = renderer
     .create(
-      <TaskItem
-        id={1}
-        status={Status.OPEN}
-        title="clean the garage"
-        setTasks={setTasks}
-      />
+      <TaskItem id={1} status={TaskStatus.OPEN} title="clean the garage" />
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
