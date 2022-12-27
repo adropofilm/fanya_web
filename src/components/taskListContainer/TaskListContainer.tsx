e34ec4c63f9bb8c47a1c1fa0d4a27b96b09e35d6
Fatima Mohamed <adropofilm@gmail.com>
2022-12-27 00:22:56 -0600

    extract redux hooks for DRY purposes & update tast list container to use them

--- src/components/taskListContainer/TaskListContainer.tsx
+++ src/components/taskListContainer/TaskListContainer.tsx
@@ -1,34 +1,35 @@
-import { useState, useEffect, ReactElement } from "react";
-import { Status, Task } from "../../types/Task.types";
-import { getTasks } from "../../utility/api";
+import { useEffect, ReactElement } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { selectTasks, fetchTasks } from "../../store/features/tasksSlice";
+import { TaskStatus, RequestStatus } from "../../types/Task.types";
 import { NewTaskInputForm } from "../newTaskInputForm/NewTaskInputForm";
 import { TaskItem } from "../taskItem/TaskItem";
 import styles from "./TaskListContainer.module.css";
 
 export const TaskListContainer = (): ReactElement => {
-  const [tasks, setTasks] = useState<ReadonlyArray<Task>>([]);
+  const dispatch = useAppDispatch();
+  const tasks = useAppSelector(selectTasks);
+  const getTasksRequestStatus = useAppSelector((state) => state.tasks.status);
 
   useEffect(() => {
-    const fetchTasks = async (): Promise<void> => {
-      try {
-        const data = await getTasks();
-        setTasks(data);
-      } catch (error) {
-        console.error(error);
+    try {
+      if (getTasksRequestStatus === RequestStatus.IDLE) {
+        dispatch(fetchTasks());
       }
-    };
-    fetchTasks();
-  }, []);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [getTasksRequestStatus, dispatch]);
 
   return (
     <div id={styles["task-list-container"]} className="flex-column-center">
       <h1 id={styles["task-list-header"]}>Keep It Moving 💪🏽</h1>
-      <NewTaskInputForm setTasks={setTasks} />
+      <NewTaskInputForm />
       <div id={styles["task-list"]} className="flex-column-center">
         {tasks.map(
           (task) =>
-            task.status === Status.OPEN && (
-              <TaskItem {...task} key={task.id} setTasks={setTasks} />
+            task.status === TaskStatus.OPEN && (
+              <TaskItem {...task} key={task.id} />
             )
         )}
       </div>
