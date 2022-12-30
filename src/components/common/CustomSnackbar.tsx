c1e814558ca142faf5a70b283ead1b8239623fcd
Fatima Mohamed <adropofilm@gmail.com>
2022-12-30 04:29:11 -0600

    add custom snackbar component + styling

---
+++ src/components/common/CustomSnackbar.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import {
+  closeSnackbar,
+  selectIsSnackbarOpen,
+  selectSnackbarContent,
+} from "../../store/features/snackbarSlice";
+import styles from "./CustomSnackbar.module.css";
+
+export const CustomSnackbar = (): ReactElement => {
+  const dispatch = useAppDispatch();
+  const isSnackbarOpen = useAppSelector(selectIsSnackbarOpen);
+  const snackbarContent = useAppSelector(selectSnackbarContent);
+
+  const handleClose = (): void => {
+    dispatch(closeSnackbar());
+  };
+
+  return (
+    <div>
+      {isSnackbarOpen && (
+        <div className={styles["snackbar"]}>
+          <div>
+            <span className={styles["snackbar-btn"]} onClick={handleClose}>
+              x
+            </span>
+            {snackbarContent}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
